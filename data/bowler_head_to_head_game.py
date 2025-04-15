e4ffd3a4160efa4ce76b49c16b99783d5375df66
mike <michael.p.vacha@gmail.com>
2025-04-14 21:21:13 -0500

    very minor changes

--- data/bowler_head_to_head_game.py
+++ data/bowler_head_to_head_game.py
@@ -7,6 +7,13 @@ from data.model_base import SqlAlchemyBase
 class Bowler_Head_To_Head_Game(SqlAlchemyBase):
     __tablename__ = "bowler_head_to_head_game_table"
 
+    # at some point I suppose you need to just make a decision and do some work, but I don't know enough about 
+    # databases to know what I should do
+    # I think it makes sense to add the game ids to this table and get rid of the scores (I don't remember if the
+    # game table holds handicaps, but it should), but if I do that, I don't know if it makes sense to keep the 
+    # bowler ids in this table - I don't know if it's desirable to have the same foreign keys on multiple different 
+    # tables - if this table has the game IDs and the bolwer IDs, then if I make a change, such as changing the order of
+    # the bowlers on one team, then I have to change both the bowler id and the game id on this table
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
 
     bowler_position: orm.Mapped[int]
--- utils/bowler_head_to_head_utils.py
+++ utils/bowler_head_to_head_utils.py
@@ -4,7 +4,25 @@ from data import db_session
 from data.bowler_head_to_head_game import Bowler_Head_To_Head_Game
 
 
-def create_bowler_head_to_head_game():
+def create_bowler_head_to_head_game(bowler_position=bowler_position,
+                                    game_number=game_number,
+                                    left_bowler_id=left_bowler_id,
+                                    right_bowler_id=right_bowler_id,
+                                    head_to_head_id=head_to_head_id,
+                                    head_to_head_game_id=head_to_head_game_id):
     #CONTINUE HERE
-    new_bowler_head_to_head_game = Bowler_Head_To_Head_Game()
+    new_bowler_head_to_head_game = Bowler_Head_To_Head_Game(bowler_position=bowler_position,
+                                                            game_number=game_number,
+                                                            left_bowler_id=left_bowler_id,
+                                                            right_bowler_id=right_bowler_id,
+                                                            head_to_head_id=head_to_head_id,
+                                                            head_to_head_game_id=head_to_head_game_id)
+    session = db_session.create_session()
+
+    try:
+        session.add(new_bowler_head_to_head)
+        session.commit()
+        return new_bowler_head_to_head_game
+    finally:
+        session.close()
 
